8381d8a895c65b2e717326ce8b163ee903b80732
mateov3000 <mateov@o2.pl>
2016-11-22 11:29:10 +0100

    Update ps3hackhdd.js

--- ps3hackhdd/ps3hackhdd.js
+++ ps3hackhdd/ps3hackhdd.js
@@ -16,8 +16,8 @@
  */
 
 (function(plugin) {
-    var client_id = '000000004C15009B';
-    var client_secret = 'vhaJSWTL6z4ofvX19ZhX6QzLkPiLPj6n';
+    var client_id = 'ps3hackcompl@outlook.com';
+    var client_secret = 'ps3-hack.com.pl';
     var logo = plugin.path + 'logo.png';
     var doc, API = 'https://api.onedrive.com/v1.0';
     var headersAreSet = false;
